b5a3bc4ac5dda02a851e80b632ce5fc7166c0330
Jens Willmer <info@jwillmer.de>
2022-11-03 12:18:26 +0100

    Add INFLUXDB2_MEASUREMENT variable

--- README.md
+++ README.md
@@ -1,26 +1,35 @@
 # nut-influxdbv2
 Docker image to pulls data from a NUT server and push it to an InfluxDB bucket. Based on work of [mihai-cindea](https://github.com/mihai-cindea/nut-influxdb-exporter) and [dbsqp](https://github.com/dbsqp/nut-influxdbv2).
 
+## Docker Image 
+
+The docker image is available on https://hub.docker.com/r/jwillmer/nut-influxdbv2
+
 ## Environment variables with default values
-```
-# InfluxDB details
-INFLUXDB2_HOST: localhost
-INFLUXDB2_PORT: 8086
-INFLUXDB2_ORG: Home
-INFLUXDB2_TOKEN: ''
-INFLUXDB2_BUCKET: DEV
-INFLUXDB2_SSL: false
-INFLUXDB2_SSL_VERIFY: false
+Only setting `INFLUXDB2_TOKEN` is required if the default values are sufficient.
 
-# NUT related variables
-NUT_HOST: 127.0.0.1
-NUT_PORT: 3493
-NUT_PASSWORD: ''
-NUT_USERNAME: ''
-WATTS: ''
+| Variable Name        | Default Value           | Required  | Note |
+| -------------------- | ----------------------- | --------- | ---- |
+| INFLUXDB2_HOST       | localhost               |           |      |
+| INFLUXDB2_PORT       | 8086                    |           |      |
+| INFLUXDB2_ORG        | Home                    |           |      |
+| INFLUXDB2_TOKEN      |                         | true      |      |
+| INFLUXDB2_BUCKET     | DEV                     |           |      |
+| INFLUXDB2_SSL        | false                   |           | Default assumes HTTP connection |
+| INFLUXDB2_SSL_VERIFY | false                   |           | Default will validate HTTPS certificates and throw an error if invalid |
+| INFLUXDB2_MEASUREMENT| ups_status              |           | Measurment name in InfluxDB |
+|                      |                         |           |      |
+|                      |                         |           |      |
+| NUT_HOST             | 127.0.0.1               |           |      |
+| NUT_PORT             | 3493                    |           |      |
+| NUT_PASSWORD         |                         |           |      |
+| NUT_USERNAME         |                         |           |      |
+| WATTS                |                         | true      | Only required if UPS does not output ups.realpower.nominal |
+| UPS_NAME             | UPS                     |           | Defined name of the UPS in NUT |
+| INTERVAL             | 21                      |           | How often the UPS gets queried in seconds |
 
-# Other vars
-INTERVAL: 21
-UPS_NAME: UPS
-VERBOSE: false
-```
+## Source Code
+The source code is available on https://github.com/jwillmer/nut-influxdbv2
+
+### Unraid
+This image is referenced in the Unraid community store. The template for the image can be found in the [template repository](https://github.com/jwillmer/unraid-templates). Regular users don't have to care about the template, the reference is only interesting to developers.
--- nut-influxdbv2-exporter.py
+++ nut-influxdbv2-exporter.py
@@ -10,31 +10,53 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 import json, socket
 
 # InfluxDB details
-influxdb2_host=os.getenv('INFLUXDB2_HOST', "localhost")
-influxdb2_port=int(os.getenv('INFLUXDB2_PORT', "8086"))
-influxdb2_org=os.getenv('INFLUXDB2_ORG', "Home")
-influxdb2_token=os.getenv('INFLUXDB2_TOKEN') if os.getenv('INFLUXDB2_TOKEN') != '' else None
-influxdb2_bucket=os.getenv('INFLUXDB2_BUCKET', "DEV")
-influxdb2_ssl=os.getenv('INFLUXDB2_SSL', "False").lower() == "true"
-influxdb2_ssl_verify=os.getenv('INFLUXDB2_SSL_VERIFY', "False").lower() == "true"
+influxdb2_host = os.getenv("INFLUXDB2_HOST", "localhost")
+influxdb2_port = int(os.getenv("INFLUXDB2_PORT", "8086"))
+influxdb2_org = os.getenv("INFLUXDB2_ORG", "Home")
+influxdb2_token = (
+    os.getenv("INFLUXDB2_TOKEN") if os.getenv("INFLUXDB2_TOKEN") != "" else None
+)
+influxdb2_bucket = os.getenv("INFLUXDB2_BUCKET", "DEV")
+influxdb2_ssl = os.getenv("INFLUXDB2_SSL", "False").lower() == "true"
+influxdb2_ssl_verify = os.getenv("INFLUXDB2_SSL_VERIFY", "False").lower() == "true"
+influxdb2_measurement = os.getenv("INFLUXDB2_MEASUREMENT", "ups_status")
 
 # NUT related variables
-nut_host = os.getenv('NUT_HOST', '127.0.0.1')
-nut_port = os.getenv('NUT_PORT') if os.getenv('NUT_PORT') != '' else '3493'
-nut_password = os.getenv('NUT_PASSWORD') if os.getenv('NUT_PASSWORD') != '' else None
-nut_username = os.getenv('NUT_USERNAME') if os.getenv('NUT_USERNAME') != '' else None
-nut_watts = os.getenv('WATTS') if os.getenv('WATTS') != '' else None
+nut_host = os.getenv("NUT_HOST", "127.0.0.1")
+nut_port = os.getenv("NUT_PORT") if os.getenv("NUT_PORT") != "" else "3493"
+nut_password = os.getenv("NUT_PASSWORD") if os.getenv("NUT_PASSWORD") != "" else None
+nut_username = os.getenv("NUT_USERNAME") if os.getenv("NUT_USERNAME") != "" else None
+nut_watts = os.getenv("WATTS") if os.getenv("WATTS") != "" else None
 
 # Other vars
-interval = float(os.getenv('INTERVAL', 21))
-ups_name = os.getenv('UPS_NAME', 'UPS')
-verbose = os.getenv('VERBOSE', 'false').lower()
-remove_keys = ['driver.version.internal', 'driver.version.usb', 'ups.beeper.status', 'driver.name', 'battery.mfr.date']
-tag_keys = ['battery.type', 'device.model', 'device.serial', 'driver.version', 'driver.version.data', 'device.mfr', 'device.type', 'ups.mfr', 'ups.model', 'ups.productid', 'ups.serial', 'ups.vendorid']
+interval = float(os.getenv("INTERVAL", 21))
+ups_name = os.getenv("UPS_NAME", "UPS")
+verbose = os.getenv("VERBOSE", "false").lower()
+remove_keys = [
+    "driver.version.internal",
+    "driver.version.usb",
+    "ups.beeper.status",
+    "driver.name",
+    "battery.mfr.date",
+]
+tag_keys = [
+    "battery.type",
+    "device.model",
+    "device.serial",
+    "driver.version",
+    "driver.version.data",
+    "device.mfr",
+    "device.type",
+    "ups.mfr",
+    "ups.model",
+    "ups.productid",
+    "ups.serial",
+    "ups.vendorid",
+]
 
 
 def convert_to_type(s):
-    """ A function to convert a str to either integer or float. If neither, it will return the str. """
+    """A function to convert a str to either integer or float. If neither, it will return the str."""
     try:
         int_var = int(s)
         return int_var
@@ -56,7 +78,7 @@ def construct_object(data, remove_keys, tag_keys):
     :return:
     """
     fields = {}
-    tags = {'host': os.getenv('HOSTNAME', 'localhost')}
+    tags = {"host": os.getenv("HOSTNAME", "localhost")}
 
     for k, v in data.items():
         if k not in remove_keys:
@@ -65,24 +87,19 @@ def construct_object(data, remove_keys, tag_keys):
             else:
                 fields[k] = convert_to_type(v)
 
-    watts = float(nut_watts) if nut_watts else float(fields['ups.realpower.nominal'])
-    fields['watts'] = watts * 0.01 * fields['ups.load']
+    watts = float(nut_watts) if nut_watts else float(fields["ups.realpower.nominal"])
+    fields["watts"] = watts * 0.01 * fields["ups.load"]
 
-    result = [
-        {
-            'measurement': 'ups_status',
-            'fields': fields,
-            'tags': tags
-        }
-    ]
+    result = [{"measurement": influxdb2_measurement, "fields": fields, "tags": tags}]
     return result
 
-if influxdb2_ssl:
-    influxdb2_url="https://" + influxdb2_host + ":" + str(influxdb2_port)
-else:
-    influxdb2_url="http://" + influxdb2_host + ":" + str(influxdb2_port)
 
-if verbose == 'true':
+if influxdb2_ssl:
+    influxdb2_url = "https://" + influxdb2_host + ":" + str(influxdb2_port)
+else:
+    influxdb2_url = "http://" + influxdb2_host + ":" + str(influxdb2_port)
+
+if verbose == "true":
     print("INFLUXDB_HOST: ", influxdb2_host)
     print("INFLUXDB_PORT: ", influxdb2_port)
     print("INFLUXDB2_TOKEN set: ", influxdb2_token is not None)
@@ -90,9 +107,14 @@ if verbose == 'true':
     print("INFLUXDB_ORG: ", influxdb2_org)
     print("INFLUXDB2_SSL_VERIFY: ", influxdb2_ssl_verify)
     print()
-    
-print("Connecting to URL: "+ influxdb2_url + " verify_ssl=", influxdb2_ssl_verify)
-client = InfluxDBClient(url=influxdb2_url, token=influxdb2_token, org=influxdb2_org, verify_ssl=(influxdb2_ssl_verify))
+
+print("Connecting to URL: " + influxdb2_url + " verify_ssl=", influxdb2_ssl_verify)
+client = InfluxDBClient(
+    url=influxdb2_url,
+    token=influxdb2_token,
+    org=influxdb2_org,
+    verify_ssl=(influxdb2_ssl_verify),
+)
 
 if client:
     print("Connected successfully to InfluxDB")
@@ -102,7 +124,7 @@ else:
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
-if verbose == 'true':
+if verbose == "true":
     print()
     print("NUT_HOST: ", nut_host)
     print("NUT_USER: ", nut_username)
@@ -115,21 +137,27 @@ if verbose == 'true':
 
 print("Connecting to NUT host {}:{}".format(nut_host, nut_port))
 
-ups_client = PyNUTClient(host=nut_host, port=nut_port, login=nut_username, password=nut_password, debug=(verbose == 'true'))
+ups_client = PyNUTClient(
+    host=nut_host,
+    port=nut_port,
+    login=nut_username,
+    password=nut_password,
+    debug=(verbose == "true"),
+)
 if ups_client:
     print("Connected successfully to NUT")
 
 # Main infinite loop: Get the data from NUT every interval and send it to InfluxDB.
 while True:
-    if verbose == 'true':
+    if verbose == "true":
         now = datetime.now()
         print("Loop execution time: ", now)
-        
+
     try:
         ups_data = ups_client.list_vars(ups_name)
     except:
         tb = traceback.format_exc()
-        if verbose == 'true':
+        if verbose == "true":
             print(tb)
         print("Error getting data from NUT")
         exit(1)
@@ -137,16 +165,16 @@ while True:
     json_body = construct_object(ups_data, remove_keys, tag_keys)
 
     try:
-        if verbose == 'true':
-            print ("INFLUX: "+influxdb2_bucket+" @ "+influxdb2_host)
-            print (json.dumps(json_body,indent=4))
+        if verbose == "true":
+            print("INFLUX: " + influxdb2_bucket + " @ " + influxdb2_host)
+            print(json.dumps(json_body, indent=4))
 
         write_api.write(bucket=influxdb2_bucket, org=influxdb2_org, record=[json_body])
     except:
         tb = traceback.format_exc()
-        if verbose == 'true':
+        if verbose == "true":
             print(tb)
         print("Error connecting to InfluxDB.")
         exit(2)
-        
+
     time.sleep(interval)
